f6e8f0b4b14540342c37f1cb4d9d7ded585eced2
Soyoung Sun <dkssud02@gmail.com>
2021-12-26 15:20:25 +0900

    index.html API 추가

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():  # put application's code here
-    return 'Hello World!'
+    # render_template 함수는 templates 폴더에 있는 html 파일을 웹브라우저에게 전달
+    return render_template('index.html')
 
 
 if __name__ == '__main__':
-    app.run()
+    # 플라스크 앱 구동 설정
+    app.run('0.0.0.0', port=9000, debug=True)
